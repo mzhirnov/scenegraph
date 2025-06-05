bc0f1d870462e537bcb1de871846e34439135846
Maksim Zhirnov <mzhirnov@gmail.com>
2025-06-05 09:56:08 +0300

    Test application

--- sgapp/main.cpp
+++ sgapp/main.cpp
@@ -1,5 +1,26 @@
 #include <scenegraph/SceneObject.h>
+#include <scenegraph/PoolAllocator.h>
+#include <iostream>
+#include <memory>
 #include <cstdio>
+
+constexpr uint32_t PopCount(uint32_t x) {
+	auto a = x - ((x >> 1) & 0x55555555);
+	auto b = (((a >> 2) & 0x33333333) + (a & 0x33333333));
+	auto c = (((b >> 4) + b) & 0x0f0f0f0f);
+	auto d = c + (c >> 8);
+	auto e = d + (d >> 16);
+	return e & 0x0000003f;
+};
+
+constexpr uint32_t Log2(uint32_t x) {
+	auto a = x | (x >> 1);
+	auto b = a | (a >> 2);
+	auto c = b | (b >> 4);
+	auto d = c | (c >> 8);
+	auto e = d | (d >> 16);
+	return PopCount(e >> 1);
+};
 
 class HelloComponent : public ComponentImpl<HelloComponent> {
 public:
@@ -7,6 +28,14 @@ public:
 	
 private:
 	friend class ComponentImpl<HelloComponent>;
+	
+	void Added(SceneObject*) noexcept {
+		puts("Added Hello");
+	}
+	
+	void Removed(SceneObject*) noexcept {
+		puts("Removed Hello");
+	}
 	
 	void Apply(SceneObject*) noexcept {
 		puts("Hello");
@@ -21,6 +50,14 @@ public:
 private:
 	friend class ComponentImpl<WorldComponent>;
 	
+	void Added(SceneObject*) noexcept {
+		puts("Added World");
+	}
+	
+	void Removed(SceneObject*) noexcept {
+		puts("Removed World");
+	}
+	
 	void Apply(SceneObject*) noexcept {
 		puts("World");
 		//Invalidate();
@@ -34,10 +71,25 @@ public:
 private:
 	friend class ComponentImpl<ExclamationComponent>;
 	
+	void Added(SceneObject*) noexcept {
+		puts("Added Exclamation");
+	}
+	
+	void Removed(SceneObject*) noexcept {
+		puts("Removed Exclamation");
+	}
+	
 	void Apply(SceneObject*) noexcept {
 		puts("!");
-		Invalidate();
+		Remove();
 	}
+};
+
+struct AutoObject {
+	AutoObject() { puts(".ctor"); }
+	~AutoObject() { puts(".dtor"); }
+	
+	int i = 0;
 };
 
 int main() {
@@ -54,6 +106,36 @@ int main() {
 	sceneObject.Update();
 	puts("---");
 	sceneObject.Update();
+	
+	PoolAllocator<AutoObject, 2> allocator;
+	
+	auto p = allocator.Allocate();
+	std::cout << p << '\n';
+	
+	{
+		auto obj = std::construct_at(allocator.Allocate());
+		std::cout << obj << '\n';
+		std::destroy_at(obj);
+		allocator.Deallocate(obj);
+	}
+	{
+		auto obj = std::construct_at(allocator.Allocate());
+		std::cout << obj << '\n';
+		std::destroy_at(obj);
+		allocator.Deallocate(obj);
+	}
+	
+	allocator.Deallocate(p);
+	
+	AutoObject *o1, *o2, *o3;
+	
+	std::cout << (o1 = allocator.Allocate()) << '\n';
+	std::cout << (o2 = allocator.Allocate()) << '\n';
+	std::cout << (o3 = allocator.Allocate()) << '\n';
+	
+	allocator.Deallocate(o1);
+	allocator.Deallocate(o2);
+	allocator.Deallocate(o3);
 	
 	return 0;
 }
