0bcaf457e70336585c80e463319fe347348078b8
Maksim Zhirnov <mzhirnov@gmail.com>
2025-06-20 13:45:16 +0300

    Better ScopeGuard

--- include/scenegraph/ScopeGuard.h
+++ include/scenegraph/ScopeGuard.h
@@ -1,17 +1,30 @@
 #pragma once
 
 #include <functional>
+#include <iostream>
+#ifdef __cpp_exceptions
 #include <exception>
+#endif
 
-template <bool OnSuccess = true, bool OnFailure = true>
+template <bool OnSuccess, bool OnFailure>
 struct ScopeGuardTag {};
 
-template <typename Handler, bool OnSuccess = true, bool OnFailure = true, typename = std::enable_if_t<std::is_invocable_v<Handler>>>
+///
+/// Scope guard
+///
+/// Calls a function object on scope exit.
+///
+template <typename Handler, bool OnSuccess = true, bool OnFailure = true>
 class ScopeGuard {
 public:
+	static_assert(OnSuccess || OnFailure, "At least one of OnSuccess or OnFailure must be true");
+	static_assert(std::is_invocable_v<Handler>, "Handler must have signature void()");
+	
 	ScopeGuard(Handler&& handler)
 		: _handler(std::forward<Handler>(handler))
+#ifdef __cpp_exceptions
 		, _exceptions(std::uncaught_exceptions())
+#endif
 	{
 	}
 	
@@ -21,25 +34,46 @@ public:
 	ScopeGuard(ScopeGuard&&) = delete;
 	ScopeGuard& operator=(ScopeGuard&&) = delete;
 	
+	// Only stack instances allowed
+	
+	static void* operator new(size_t, ...) = delete;
+	static void operator delete(void*, ...) = delete;
+	
+	static void* operator new[](size_t, ...) = delete;
+	static void operator delete[](void*, ...) = delete;
+	
 	~ScopeGuard() {
-		int exceptions = std::uncaught_exceptions();
+		if (_cancelled) {
+			return;
+		}
 		
-		if ((OnSuccess && exceptions == _exceptions) ||
-			(OnFailure && exceptions != _exceptions))
-		{
-			if (!_commit) {
+#ifdef __cpp_exceptions
+		const bool exception = std::uncaught_exceptions() != _exceptions;
+		
+		if ((OnSuccess && !exception) || (OnFailure && exception)) {
+			try {
 				std::invoke(_handler);
 			}
+			catch (...) {
+				std::cerr << "Exception thrown during scope exit" << std::endl;
+				throw;
+			}
 		}
+#else
+		if constexpr (OnSuccess) {
+			std::invoke(_handler);
+		}
+#endif
 	}
 	
-	// If commited, handler won't be called
-	void Commit() noexcept { _commit = true; }
+	void Cancel() noexcept { _cancelled = true; }
 
 private:
 	Handler _handler;
+#ifdef __cpp_exceptions
 	int _exceptions = 0;
-	bool _commit = false;
+#endif
+	bool _cancelled = false;
 };
 
 template <typename Handler, bool OnSuccess, bool OnFailure>
@@ -57,10 +91,22 @@ constexpr auto operator+(ScopeGuardTag<OnSuccess, OnFailure>, Handler&& handler)
 #endif
 
 #define ON_SCOPE_EXIT(...) \
-	auto UNIQUE_VARIABLE(SCOPE_EXIT_handler_) = ScopeGuardTag{} + [__VA_ARGS__]()
+	ScopeGuard UNIQUE_VARIABLE(SCOPE_EXIT_handler_) = [__VA_ARGS__]()
 	
 #define ON_SCOPE_EXIT_SUCCESS(...) \
 	auto UNIQUE_VARIABLE(SCOPE_EXIT_success_handler_) = ScopeGuardTag<true, false>{} + [__VA_ARGS__]()
 	
 #define ON_SCOPE_EXIT_FAILURE(...) \
 	auto UNIQUE_VARIABLE(SCOPE_EXIT_failure_handler_) = ScopeGuardTag<false, true>{} + [__VA_ARGS__]()
+
+#ifdef __cpp_exceptions
+	#define TRY try
+	#define CATCH(...) catch(__VA_ARGS__)
+	#define CATCH_ALL() catch(...)
+	#define THROW(...) throw __VA_ARGS__;
+#else
+	#define TRY
+	#define CATCH(...) while (false)
+	#define CATCH_ALL() while (false)
+	#define THROW(...) (void)0;
+#endif
