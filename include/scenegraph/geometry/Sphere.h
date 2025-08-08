a6d17fdd00d8d0ed59a2adeaaded98780332e866
Maksim Zhirnov <mzhirnov@gmail.com>
2025-08-08 13:04:41 +0300

    Added sphere functions

--- include/scenegraph/geometry/Sphere.h
+++ include/scenegraph/geometry/Sphere.h
@@ -6,3 +6,13 @@ struct Sphere {
 	Vector3 origin;
 	float radius;
 };
+
+inline Sphere SphereMake(Vector3 origin, float radius) {
+	Sphere out;
+	out.origin = origin;
+	out.radius = radius;
+	return out;
+}
+
+inline bool SphereDistance(const Sphere& s1, const Sphere& s2) { return Vector3Distance(s1.origin, s2.origin) - (s1.radius + s2.radius); }
+inline bool SphereIntersect(const Sphere& s1, const Sphere& s2) { return SphereDistance(s1.origin, s2.origin) < 0; }
