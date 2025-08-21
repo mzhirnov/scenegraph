c751cfe5ab353a41af3f8cc322e634ac3dcd29a5
Maksim Zhirnov <mzhirnov@gmail.com>
2025-08-21 09:15:09 +0300

    Better NonCopyable

--- include/scenegraph/utils/NonCopyable.h
+++ include/scenegraph/utils/NonCopyable.h
@@ -1,17 +1,52 @@
 #pragma once
 
+///
+/// Non Copyable
+///
 class NonCopyable {
 public:
 	NonCopyable() = default;
 	
 	NonCopyable(const NonCopyable&) = delete;
 	NonCopyable& operator=(const NonCopyable&) = delete;
+	
+	NonCopyable(NonCopyable&&) = default;
+	NonCopyable& operator=(NonCopyable&&) = default;
+	
+protected:
+	~NonCopyable() = default;
 };
 
-class NonCopyableNonMovable : public NonCopyable {
+///
+/// Non Movable
+///
+class NonMovable {
+public:
+	NonMovable() = default;
+	
+	NonMovable(const NonMovable&) = default;
+	NonMovable& operator=(const NonMovable&) = default;
+	
+	NonMovable(NonMovable&&) = delete;
+	NonMovable& operator=(NonMovable&&) = delete;
+	
+protected:
+	~NonMovable() = default;
+};
+
+///
+/// Non Copyable Non Movable
+///
+class NonCopyableNonMovable {
 public:
 	NonCopyableNonMovable() = default;
 	
+	NonCopyableNonMovable(const NonCopyableNonMovable&) = delete;
+	NonCopyableNonMovable& operator=(const NonCopyableNonMovable&) = delete;
+	
 	NonCopyableNonMovable(NonCopyableNonMovable&&) = delete;
 	NonCopyableNonMovable& operator=(NonCopyableNonMovable&&) = delete;
+	
+protected:
+	~NonCopyableNonMovable() = default;
 };
